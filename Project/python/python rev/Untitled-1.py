6df14569fdd3dbad7e123cccc645b6a456751c3b
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2026-04-03 16:22:24 +0530

    Merge remote changes

--- Project/python/python rev/Untitled-1.py
+++ Project/python/python rev/Untitled-1.py
@@ -15,7 +15,7 @@ while n<=5:
 
 
 
-L1 = input("Enter the List of Numbers: ")
+"""L1 = input("Enter the List of Numbers: ")
 print(L1)
 L1 = L1.split(",")
 
@@ -23,8 +23,31 @@ print(L1)
 L1.sort()
 print(L1)
 
+"""
+
+"""L1 = input("Enter the List of Numbers: ")
+L1 = L1.split(",")
+
+print(L1)"""
 
 
+
+
+def caught_speeding(speed, is_birthday):
+
+    if is_birthday:
+        speed -= 5   # 🎯 this is the +5 tolerance
+
+    if speed <= 60:
+        return "No Ticket"
+    elif speed <= 80:
+        return "Small Ticket"
+    else:
+        return "Big Ticket"
+    
+    
+    
+print(caught_speeding(64, 3/4))
 
     
     
